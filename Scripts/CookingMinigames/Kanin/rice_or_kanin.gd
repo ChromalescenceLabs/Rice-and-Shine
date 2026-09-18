3f53ba1b71bf8e3cd88d733caf274a3496548337
Jazzminum <jhazmineaudrey7@gmail.com>
2026-09-18 20:57:29 +0800

    feat: add ricecup entry animation

--- Scripts/CookingMinigames/Kanin/rice_or_kanin.gd
+++ Scripts/CookingMinigames/Kanin/rice_or_kanin.gd
@@ -3,11 +3,18 @@ extends Node2D
 @onready var rice_cont_closed: TextureButton = $RiceCups/RiceCont/RiceContClosed
 @onready var rice_cont_opened: TextureRect = $RiceCups/RiceCont/RiceContOpened
 
+@onready var water: Node2D = $Water
+
 const SUCCIND = preload("uid://beppy36ki8xum")
 
 const RICE_CUP = preload("uid://cpvm4q1ywd7h5")
 var rice_cup_pos : Vector2
 var riceContMonitoring : bool = false
+
+func _ready() -> void:
+	rice_cups.position.x += get_viewport().get_visible_rect().size.x
+	
+	item_enter(rice_cups)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Click"):
@@ -35,3 +42,15 @@ func _on_rice_bowl_rice_cup_done() -> void:
 	var success = SUCCIND.instantiate()
 	success.global_position = get_viewport().size / 2
 	add_child(success)
+	
+	await success.tree_exited
+	
+	item_exit(rice_cups)
+	
+func item_enter(item) -> void:
+	create_tween().tween_property(item, "position", Vector2(0,0), 2).set_trans(Tween.TRANS_ELASTIC)
+	
+func item_exit(item) -> void:
+	var vw = get_viewport().get_visible_rect().size.x
+	create_tween().tween_property(item, "position", Vector2(vw * -1, 0), 2).set_trans(Tween.TRANS_ELASTIC)
+	
