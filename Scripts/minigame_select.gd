0dd9f1a8c86dd408010ed41b3fe8f071a26c5b0c
Clif <cliffordlukedonato@gmail.com>
2026-09-18 23:13:14 +0800

    feat: preparation for pancake coding

--- Scripts/minigame_select.gd
+++ Scripts/minigame_select.gd
@@ -16,7 +16,7 @@ func _on_minigame_1_pressed() -> void:
 
 
 func _on_minigame_2_pressed() -> void:
-	pass # Replace with function body.
+	SceneLoader.load_scene("uid://bxwlgly5o4vd3", 1.5)
 
 
 func _on_minigame_3_pressed() -> void:
