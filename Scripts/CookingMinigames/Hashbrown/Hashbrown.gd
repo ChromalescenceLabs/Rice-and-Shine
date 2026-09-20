36217f80935fb80b287f91ba6791236c88cdd90d
alliumC <chloe.marguerite.pabon1909@gmail.com>
2026-09-20 19:58:33 +0800

    ft. added images
    heres my beautiful potatoes thanks. I deleted the other one bc of trauma , thanks

--- Scripts/CookingMinigames/Hashbrown/Hashbrown.gd
+++ Scripts/CookingMinigames/Hashbrown/Hashbrown.gd
@@ -1,45 +1,7 @@
 extends Node2D
-@onready var line_2d: Line2D = $Line2D
-@onready var peeler: Area2D = $Peeler
-@onready var p_potato: TextureRect = $Line2D/AreaPPotato/PPotato
-@onready var p_particles: CPUParticles2D = $Peeler/PParticles
-
-
-var peeling: bool = false
-var isPeeler = false
-
 
 func _ready() -> void:
-	p_potato.visible=false
+	pass
 
 func _process(delta: float) -> void:
-	if peeling:
-		peeler.global_position = get_global_mouse_position()
-		
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and peeling:
-		line_2d.add_point(line_2d.to_local(event.position))
-
-	if event.is_action_released("Click") and isPeeler:
-		peeling = true
-
-func _on_peeler_mouse_entered() -> void:
-	isPeeler = true
-
-func _on_peeler_mouse_exited() -> void:
-	isPeeler = false
-
-func _on_area_p_potato_mouse_entered() -> void:
-	if isPeeler:
-		p_potato.visible=true
-		
-		for i in line_2d.points.size() - 1:
-			var new_shape = CollisionShape2D.new()
-			$Line2D/AreaLine.add_child(new_shape)
-			var rect = RectangleShape2D.new()
-			new_shape.position = (line_2d.points[i-1] + line_2d.points[i]) / 2
-			new_shape.rotation = line_2d.points[i-1].direction_to(line_2d.points[i]).angle()
-			var length = line_2d.points[i-1].distance_to(line_2d.points[i])
-			rect.size = Vector2(length / 2, 10)
-			new_shape.shape = rect
+	pass
